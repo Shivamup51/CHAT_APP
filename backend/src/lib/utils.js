50a3c9345189edca64b54eede4d23a2bca65bc3e
Shivamup51 <shivamupadhyay2868@gmail.com>
2025-05-07 23:54:14 +0530

    Update cookie settings in utils.js to enhance security by using secure cookies in production and adjusting sameSite attribute based on environment.

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "Lax", // Changed from "Strict"
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 'None' for cross-site, 'Lax' for development
     path: "/",
   });
 
